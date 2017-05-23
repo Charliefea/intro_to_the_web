feccf3b8f5df929886f02c3f415dba26f437d63c
Sam Pritchard <sampritchard@msn.com>
2017-05-23 12:18:26 +0100

    added a ERB tags to randomise name

--- app.rb
+++ app.rb
@@ -4,26 +4,6 @@ get '/' do
   "Hello World"
 end
 
-get '/secret' do
-  "Charlotte and Jade were here!"
-end
-
-get '/test' do
-  'Test 123 test 123'
-end
-
-get '/jade' do
-  "she is awesome"
-end
-
-get '/charlotte' do
-  "Is not quite as awesome as Jade"
-end
-
-get '/makers' do
-  "Why is it so difficult"
-end
-
 get '/cat' do
   erb(:index)
 end
